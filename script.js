6e06b576137ec627028f9f899476bc300dc2b17b
Ronald Nogueira <ronaldnogueiramax10@gmail.com>
2022-05-22 21:21:07 -0300

    s

--- script.js
+++ script.js
@@ -16,58 +16,28 @@ btnMobile.addEventListener('touchstart', toggleMenu);
 
 
 
-var balls = document.querySelector('.balls')
-		var quant = document.querySelectorAll('.slides .images')
-		var atual = 0
-		var imagem = document.getElementById('atual')
-		var next = document.getElementById('next')
-		var voltar = document.getElementById('voltar')
-		var rolar = true
 
-		for(let i=0; i < quant.length; i++){
-			var div = document.createElement('div')
-			div.id = i
-			balls.appendChild(div)
-		}
-		document.getElementById('0').classList.add('imgAtual')
 
-		var pos = document.querySelectorAll('.balls div')
 
-		for(let i=0; i< pos.length; i++){
-			pos[i].addEventListener('click', function(){
-				atual = pos[i].id
-				rolar = false
-				slide()
-			})
-		}
 
-		voltar.addEventListener('click', ()=>{
-			atual--
-			rolar = false
-			slide()
-		})
-		next.addEventListener('click', ()=>{
-			atual++
-			rolar = false
-			slide()
-		})
 
-		function slide(){
-			if(atual >= quant.length){
-				atual = 0
-			}else if(atual < 0){
-				atual = quant.length-1
-			}
-			document.querySelector('.imgAtual').classList.remove('imgAtual')
-			imagem.style.marginLeft = -1024*atual+'px'
-			document.getElementById(atual).classList.add('imgAtual')
-		}
-		/*slide()*/
-		setInterval(()=>{
-			if(rolar){
-			atual++
-			slide()
-		}else{
-			rolar = true
-		}
-		},5000)
+
+
+
+
+
+		var swiper = new Swiper(".mySwiper", {
+			slidesPerView: 3,
+			spaceBetween: 30,
+			slidesPerGroup: 3,
+			loop: true,
+			loopFillGroupWithBlank: true,
+			pagination: {
+			  el: ".swiper-pagination",
+			  clickable: true,
+			},
+			navigation: {
+			  nextEl: ".swiper-button-next",
+			  prevEl: ".swiper-button-prev",
+			},
+		  });
